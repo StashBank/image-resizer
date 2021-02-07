937cdf8649b91dbfe201647fb69b8781b88109fc
Stash Bank <o.pavlovskyi@gmail.com>
2021-02-08 00:03:22 +0200

    promise

--- index.js
+++ index.js
@@ -12,44 +12,38 @@ if (!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir);
 }
 
+function resize(path, width, height, outputPath) {
+  return new Promise((resolve, reject) => {
+
+    sharp(path)
+      .resize(width, height)
+      .toFile(outputFilePath, (error, data) => {
+        if (error) {
+          reject(error);
+        } else {
+          console.log(`Processed ${outputPath}`);
+          resolve();
+        }
+      });
+  })
+}
+
 const imageFiles = files
   .filter(fileName => /jpg$|jpeg$|png$/i.test(fileName))
   .map(file => path.join(inputDir, file));
 
-const prom = new Promise((resolve, reject) => {
-
-const queue = [];
-
-function resize(path, width, height, outputPath) {
-  sharp(path)
-    .resize(width, height)
-    .toFile(outputFilePath, (error, data) => {
-      if (error) {
-        console.error(error);
-      } else {
-        //fs.writeFileSync(outputFilePath, data);
-        queue.splice(queue.findIndex(x => x === outputFilePath));
-        console.log(`Processed ${outputPath}`);
-        if (queue.length === 0) {
-          resolve();
-        }
-      }
-    });
-}
-
-imageFiles.forEach(imagePath => targetResolutions
+const prom = Promise.all(
+  imageFiles.map(imagePath => targetResolutions
     .map(x => x.split('x').map(v => +v))
-    .forEach(resolution => {
+    .map(async resolution => {
       const [width, height] = resolution;
       const fileInfo = path.parse(imagePath);
       const outputFileName = `${fileInfo.name}_${width}x${height}${fileInfo.ext}`;
       outputFilePath = path.join(outputDir, outputFileName);
-      resize(imagePath, width, height, outputFilePath);
-      queue.push(outputFilePath);
+      await resize(imagePath, width, height, outputFilePath);
     })
-);
+));
 
-});
 await prom;
 
 }
